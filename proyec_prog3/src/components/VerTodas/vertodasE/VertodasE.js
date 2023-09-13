c588f0f7ae1bc4c91bdd155a3bf8f6e35031551d
julighio <jghiorivas@udesa.edu.ar>
2023-09-13 17:03:11 -0300

    Hicimos el cargar mas y menos en ver todas

--- proyec_prog3/src/components/VerTodas/vertodasE/VertodasE.js
+++ proyec_prog3/src/components/VerTodas/vertodasE/VertodasE.js
@@ -9,6 +9,16 @@ class VertodasE extends Component{
             peliculasestreno:[]
         }
     }
+    cargarMas(){
+        this.setState({
+            cargar : true
+        })
+      }
+      cargarMenos(){
+        this.setState({
+            cargar:false
+        })
+      }
     componentDidMount(){
         fetch("https://api.themoviedb.org/3/movie/upcoming?api_key=75196a6b12119e0621f7373e3de1a94a")
         .then(res=>res.json())
@@ -20,14 +30,32 @@ class VertodasE extends Component{
 render(){
     return(
         <React.Fragment>
-
-                <h2 className="barras"> Proximos estrenos:</h2>
-
-                <section className="peliculaspop">
-                    {this.state.peliculasestreno.slice(0,20).map((elem,idx)=> <PeliculaEstreno key= {elem + idx} datospelicula={elem}/>)}
+                    <h2 className="barras"> Proximos estrenos: </h2>
+                    <section className="peliculaspop">
+                    
+                        {
+                            this.state.cargar === true?
+                            
+                            <React.Fragment>
+                                <section className="peliculaspop">
+                                    {this.state.peliculasestreno.map((elem,idx)=> <PeliculaEstreno key= {elem + idx} datospelicula={elem}/>)}
+                                </section>
+                                <button className='botonP' onClick={()=> this.cargarMenos()}>Mostrar menos</button>
+                            </React.Fragment>
+                            :
+                            <>
+                            <React.Fragment>
+                            
+                                <section className="peliculaspop">
+                                    {this.state.peliculasestreno.slice(0,9).map((elem,idx)=> <PeliculaEstreno key= {elem + idx} datospelicula={elem}/>)}
+                                </section>
+                                <button className='botonP' onClick={()=> this.cargarMas()}>Mostrar más</button>
+                            </React.Fragment>
+                            </>
+                        }
+                    
                 </section>
-
-        </React.Fragment>
+            </React.Fragment>
     )
 }
 }
--- proyec_prog3/src/components/VerTodas/vertodasP/VertodasP.js
+++ proyec_prog3/src/components/VerTodas/vertodasP/VertodasP.js
@@ -10,6 +10,16 @@ class VertodasP extends Component{
             peliculas: []
         }
      }
+     cargarMas(){
+        this.setState({
+            cargar : true
+        })
+      }
+      cargarMenos(){
+        this.setState({
+            cargar:false
+        })
+      }
      componentDidMount(){
         fetch("https://api.themoviedb.org/3/movie/popular?api_key=75196a6b12119e0621f7373e3de1a94a ")
         .then(res=> res.json())
@@ -20,13 +30,31 @@ class VertodasP extends Component{
      render(){
         return(
             <React.Fragment>
-    
                     <h2 className="barras"> Todas las peliculas populares: </h2>
-    
                     <section className="peliculaspop">
-                        {this.state.peliculas.slice(0,20).map((elem,idx)=> <Pelicula key= {elem + idx} datospelicula={elem}/>)}
-                    </section>
-    
+                    
+                        {
+                            this.state.cargar === true?
+                            
+                            <React.Fragment>
+                                <section className="peliculaspop">
+                                    {this.state.peliculas.map((elem,idx)=> <Pelicula key= {elem + idx} datospelicula={elem}/>)}
+                                </section>
+                                <button className='botonP' onClick={()=> this.cargarMenos()}>Mostrar menos</button>
+                            </React.Fragment>
+                            :
+                            <>
+                            <React.Fragment>
+                            
+                                <section className="peliculaspop">
+                                    {this.state.peliculas.slice(0,9).map((elem,idx)=> <Pelicula key= {elem + idx} datospelicula={elem}/>)}
+                                </section>
+                                <button className='botonP' onClick={()=> this.cargarMas()}>Mostrar más</button>
+                            </React.Fragment>
+                            </>
+                        }
+                    
+                </section>
             </React.Fragment>
         )
     }
